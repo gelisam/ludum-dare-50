ab9867691d3d1d14c8db5cc8c2d80c0b9f08d52e
Samuel Gélineau <gelisam+github@gmail.com>
2022-04-01 22:01:37 -0400

    my first sound effect

--- src/LudumDare.hs
+++ src/LudumDare.hs
@@ -1,6 +1,43 @@
+{-# LANGUAGE ImportQualifiedPost, NumericUnderscores #-}
 module LudumDare (main) where
+
+import Control.Concurrent (threadDelay)
+import Control.Exception (bracket, finally)
+import SDL qualified
+import SDL.Mixer qualified as Mixer
+
+
+withLoadable
+  :: Mixer.Loadable loadable
+  => FilePath
+  -> (loadable -> IO a)
+  -> IO a
+withLoadable filePath
+  = bracket
+      (Mixer.load filePath)
+      (\chunk -> do
+         Mixer.halt Mixer.AllChannels  -- docs say not to free while playing
+         Mixer.free chunk)
+
+withChunk
+  :: FilePath
+  -> (Mixer.Chunk -> IO a)
+  -> IO a
+withChunk
+  = withLoadable
+
+withMusic
+  :: FilePath
+  -> (Mixer.Music -> IO a)
+  -> IO a
+withMusic
+  = withLoadable
 
 main
   :: IO ()
 main = do
-  putStrLn "typechecks."
+  SDL.initializeAll
+  Mixer.withAudio Mixer.defaultAudio 1024 $ do
+    withChunk "move.wav" $ \moveChunk -> do
+      Mixer.play moveChunk
+      threadDelay 1_000_000
