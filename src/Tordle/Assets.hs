31996014c07cb9b59c4edbcb92f7e829f9ced6fc
Samuel Gélineau <gelisam+github@gmail.com>
2022-04-17 23:25:06 -0400

    group similar asset-loading code

--- src/Tordle/Assets.hs
+++ src/Tordle/Assets.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ImportQualifiedPost, OverloadedStrings #-}
+{-# OPTIONS -Wno-incomplete-uni-patterns #-}
 module Tordle.Assets where
 
 import Data.Function.Extra (With(..), withMultiple)
@@ -39,43 +40,52 @@ withAssets
   -> (Assets -> IO a)
   -> IO a
 withAssets renderer body = do
-  withFont "assets/clear-sans.regular.ttf" 50 $ \titleFont -> do
-    withFont "assets/clear-sans.regular.ttf" 24 $ \letterFont -> do
-      withSoundEffect "assets/move.wav" $ \moveSoundEffect -> do
-        withSoundEffect "assets/lose.wav" $ \gameOverSoundEffect -> do
-          withTextTexture renderer titleFont (V4 0 0 0 255) "Tordle" $ \titleTexture -> do
-            withTextTexture renderer titleFont (V4 0 0 0 255) "Game Over" $ \gameOverTexture -> do
-              withTextTexture renderer titleFont (V4 0 0 0 255) "Congratulations!" $ \winTexture -> do
-                let allChars = '?' : allLetters
-                withMultiple
-                    [ With $ withTextTexture renderer letterFont (V4 0 0 0 255) (Text.singleton c)
-                    | c <- allChars
-                    ]
-                    $ \blackLetterTextures -> do
-                  withMultiple
-                      [ With $ withTextTexture renderer letterFont (V4 255 255 255 255) (Text.singleton c)
-                      | c <- allChars
-                      ]
-                      $ \whiteLetterTextures -> do
-                    commonWords <- readFile "assets/common-words.txt"
-                    allWords <- readFile "assets/all-words.txt"
-                    body $ Assets
-                      { assetsTitleTexture
-                          = titleTexture
-                      , assetsGameOverTexture
-                          = gameOverTexture
-                      , assetsWinTexture
-                          = winTexture
-                      , assetsBlackLetterTextures
-                          = Map.fromList $ zip allChars blackLetterTextures
-                      , assetsWhiteLetterTextures
-                          = Map.fromList $ zip allChars whiteLetterTextures
-                      , assetsMoveSoundEffect
-                          = moveSoundEffect
-                      , assetsGameOverSoundEffect
-                          = gameOverSoundEffect
-                      , assetsCommonWords
-                          = Set.fromList $ lines commonWords
-                      , assetsAllWords
-                          = Set.fromList $ lines allWords
-                      }
+  withMultiple
+      [ With $ withFont "assets/clear-sans.regular.ttf" 50
+      , With $ withFont "assets/clear-sans.regular.ttf" 24
+      ]
+      $ \[titleFont, letterFont] -> do
+    withMultiple
+        [ With $ withSoundEffect "assets/move.wav"
+        , With $ withSoundEffect "assets/lose.wav"
+        ]
+        $ \[moveSoundEffect, gameOverSoundEffect] -> do
+      withMultiple
+          [ With $ withTextTexture renderer titleFont (V4 0 0 0 255) "Tordle"
+          , With $ withTextTexture renderer titleFont (V4 0 0 0 255) "Game Over"
+          , With $ withTextTexture renderer titleFont (V4 0 0 0 255) "Congratulations!"
+          ]
+          $ \[titleTexture, gameOverTexture, winTexture] -> do
+        let allChars = '?' : allLetters
+        withMultiple
+            [ With $ withTextTexture renderer letterFont (V4 0 0 0 255) (Text.singleton c)
+            | c <- allChars
+            ]
+            $ \blackLetterTextures -> do
+          withMultiple
+              [ With $ withTextTexture renderer letterFont (V4 255 255 255 255) (Text.singleton c)
+              | c <- allChars
+              ]
+              $ \whiteLetterTextures -> do
+            commonWords <- readFile "assets/common-words.txt"
+            allWords <- readFile "assets/all-words.txt"
+            body $ Assets
+              { assetsTitleTexture
+                  = titleTexture
+              , assetsGameOverTexture
+                  = gameOverTexture
+              , assetsWinTexture
+                  = winTexture
+              , assetsBlackLetterTextures
+                  = Map.fromList $ zip allChars blackLetterTextures
+              , assetsWhiteLetterTextures
+                  = Map.fromList $ zip allChars whiteLetterTextures
+              , assetsMoveSoundEffect
+                  = moveSoundEffect
+              , assetsGameOverSoundEffect
+                  = gameOverSoundEffect
+              , assetsCommonWords
+                  = Set.fromList $ lines commonWords
+              , assetsAllWords
+                  = Set.fromList $ lines allWords
+              }
