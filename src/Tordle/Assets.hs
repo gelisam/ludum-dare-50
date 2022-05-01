18b68b2a4bb6770c2b527c2e694ee4edc08bb0c9
Samuel Gélineau <gelisam+github@gmail.com>
2022-04-30 23:32:22 -0400

    do not load the word list
    I don't yet know how to load files in wasm, so attempting to load the
    word list is currently crashing the program.

--- src/Tordle/Assets.hs
+++ src/Tordle/Assets.hs
@@ -1,31 +1,13 @@
-{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
-{-# OPTIONS -Wno-incomplete-uni-patterns #-}
 module Tordle.Assets where
 
-import Data.Set (Set)
-import qualified Data.Set as Set
-import GHC.Generics (Generic)
 import SDL.Video.Renderer.Shim (Renderer)
 
 
-data Assets = Assets
-  { assetsCommonWords
-      :: Set String
-  , assetsAllWords
-      :: Set String
-  }
-  deriving (Generic)
+data Assets = AssetsStub
 
 withAssets
   :: Renderer
   -> (Assets -> IO a)
   -> IO a
 withAssets _renderer body = do
-  commonWords <- readFile "assets/common-words.txt"
-  allWords <- readFile "assets/all-words.txt"
-  body $ Assets
-    { assetsCommonWords
-        = Set.fromList $ lines commonWords
-    , assetsAllWords
-        = Set.fromList $ lines allWords
-    }
+  body AssetsStub
--- src/Tordle/Rng.hs
+++ src/Tordle/Rng.hs
@@ -2,13 +2,10 @@
 module Tordle.Rng where
 
 import Control.Monad.State
-import Control.Monad.Writer
-import Data.Foldable
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Traversable
 import System.Random.Stateful (StateGenM(..), StdGen, uniformRM)
-import Tordle.Assets
 import Tordle.Model
 
 
@@ -34,46 +31,6 @@ randomMember
 randomMember xs = do
   i <- uniformRM (0, Set.size xs - 1) StateGenM
   pure (Set.elemAt i xs)
-
-randomWord
-  :: MonadState StdGen m
-  => Assets
-  -> m String
-randomWord
-  = randomMember
-  . assetsCommonWords
-
-randomCompatibleWord
-  :: MonadState StdGen m
-  => Assets
-  -> [Label]
-  -> m (Maybe String)
-randomCompatibleWord (Assets {assetsAllWords}) labels = do
-  compatibleWords <- execWriterT $ do
-    for_ assetsAllWords $ \potentialWord -> do
-      when (isCompatibleWord potentialWord) $ do
-        tell [potentialWord]
-  case compatibleWords of
-    [] -> do
-      pure Nothing
-    _ -> do
-      Just <$> randomElement compatibleWords
-  where
-    isCompatibleLetter
-      :: Label
-      -> Char
-      -> Bool
-    isCompatibleLetter Wild _
-      = True
-    isCompatibleLetter (Letter c1) c2
-      = c1 == c2
-
-    isCompatibleWord
-      :: String
-      -> Bool
-    isCompatibleWord
-      = all (uncurry isCompatibleLetter)
-      . zip labels
 
 randomCompatibleGibberish
   :: MonadState StdGen m
