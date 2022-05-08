1b60e6ed3d99dcb5c9032942c443ad4178fdd1ff
Samuel Gélineau <gelisam+github@gmail.com>
2022-05-08 19:53:05 -0400

    draw the rectangle with the right color

--- src/SDL/Primitive/Asterius.hs
+++ src/SDL/Primitive/Asterius.hs
@@ -13,12 +13,24 @@ import SDL.Video.Asterius.Types
 
 
 foreign import javascript
+  "(() => {                                                                \
+  \  $1.strokeStyle = 'rgba(' + $2 + ',' + $3 + ',' + $4 + ',' + $5 + ')'; \
+  \})()"
+  js_setStrokeColor
+    :: Renderer
+    -> Word8  -- ^ red, 0-255
+    -> Word8  -- ^ green, 0-255
+    -> Word8  -- ^ blue, 0-255
+    -> Word8  -- ^ alpha, 0-255
+    -> IO ()
+
+foreign import javascript
   "(() => {                   \
   \  $1.beginPath();          \
   \  $1.rect($2, $3, $4, $5); \
   \  $1.stroke();             \
   \})()"
-  js_drawRect
+  js_strokeRect
     :: Renderer
     -> Int  -- ^ top-left x
     -> Int  -- ^ top-left y
@@ -29,6 +41,13 @@ foreign import javascript
 
 type Color = V4 Word8
 type Pos = V2 CInt
+
+setStrokeColor
+  :: Renderer
+  -> Color
+  -> IO ()
+setStrokeColor renderer (V4 r g b a) = do
+  js_setStrokeColor renderer r g b a
 
 fillRectangle
   :: Renderer
@@ -45,10 +64,11 @@ rectangle
   -> Pos
   -> Color
   -> IO ()
-rectangle renderer (V2 x1 y1) (V2 x2 y2) _ = do
+rectangle renderer (V2 x1 y1) (V2 x2 y2) color = do
   let w = x2 - x1
   let h = y2 - y1
-  js_drawRect renderer
+  setStrokeColor renderer color
+  js_strokeRect renderer
     (fromIntegral x1)
     (fromIntegral y1)
     (fromIntegral w)
